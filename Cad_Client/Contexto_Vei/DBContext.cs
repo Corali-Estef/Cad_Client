9b194519a15e2cc608276342a425f81442b48bf7
Corali Estephani <47872480+Corali-Estef@users.noreply.github.com>
2020-03-30 13:36:53 -0300

    Add files via upload

---
+++ Cad_Client/Contexto_Vei/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using Veiculos.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ClienteVeiculos.Contexto
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Placa_veiculo> Placa_veiculo { get; set; }
+
+        public DbSet<Horario_entrada> Horario_entrada{ get; set; }
+
+        public DbSet<Horario_saida> Horario_saida{ get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
---
+++ Cad_Client/Veiculo Cliente/ClienteController.cs
@@ -0,0 +1,41 @@
+﻿using cadastro.veiculos;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cadastro.veiculos
+{
+    public class ClienteVeiculos : Veiculos
+    {
+        private DBContext db = new DBContext();
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(ClienteVeiculos));
+
+        #region Actions
+
+        // Cliente
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Clientes";
+            return View();
+        }
+
+
+        public ActionResult Listar(string nome)
+        {
+            var lista = new List<Cliente>();
+
+            try
+            {
+                if (string.IsNullOrEmpty(nome))
+                    lista = db.Clientes.ToList();
+                else
+                    lista = db.Clientes.Where(m => m.Nome.Contains(nome)).ToList();
+            }
+            catch (Exception ex)
+            {
+                //TODO: LOG   
+                log.Error(ex.Message);
+            }
+
+            return PartialView(" ", lista);
+        }
